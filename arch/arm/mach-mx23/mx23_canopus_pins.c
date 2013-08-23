292f072e43692c68609512237318396437ab41a3
YongSung Lee <yslee@udcsystems.com>
2013-08-23 14:59:41 +0900

    [UD 5773] pins : re-enable AUART1 pin
    TP board is same as ES board and use AUART1 port for console output.
    Rollback ...
    
    This reverts commit 8f267a06daa3359ebb51f0160a629301777b2ff1.
    
    Change-Id: I05813ee94e8e4243f08be3945c4fe01cd8bf1a23

--- arch/arm/mach-mx23/mx23_canopus_pins.c
+++ arch/arm/mach-mx23/mx23_canopus_pins.c
@@ -51,13 +51,13 @@ static struct pin_desc canopus_fixed_pins[] = {
 #ifdef CONFIG_MXS_AUART1_DEVICE_ENABLE
 	{
 		.name		= "AUART1.RX",
-		.id		= PINID_AUART1_RX,
-		.fun		= PIN_FUN1,
+		.id		= PINID_I2C_SDA,
+		.fun		= PIN_FUN3,
 	},
 	{
 		.name		= "AUART1.TX",
-		.id		= PINID_AUART1_TX,
-		.fun		= PIN_FUN1,
+		.id		= PINID_I2C_SCL,
+		.fun		= PIN_FUN3,
 	},
 #endif
 #if defined(CONFIG_FB_MXS) || defined(CONFIG_FB_MXS_MODULE)
